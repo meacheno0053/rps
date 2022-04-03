b4be770bb11b5d187c645f4326e37dc3ddc0364a
meacheno0053 <99371795+meacheno0053@users.noreply.github.com>
2022-04-03 22:59:41 +0000

    created user checker #2 adding in responses, will need to fix errors

--- 02_user_choice_v2.py
+++ 02_user_choice_v2.py
@@ -1,14 +1,23 @@
-# Funtions go here
-def choice_checker(question):
+# Version 2 - error message included when calling function
 
-  error = "please chose form rock / paper / " \
-            "scissors (or xxx to quit)"
+
+# Funtions go here
+def choice_checker(question, valid_list, error):
  
   valid = False
   while not valid:
     
     # Ask user for choice (and put choice in lowercase)
     response = input(question).lower()
+
+    #iterates through list and if response is an item
+    #in the list (or the first letter of an item), the
+    #full item name is returned        
+    for item in vslid_list:
+      if response == item [0] or response == item:
+        return item
+
+      #outpost error if item not in list
 
     if response == "r" or response == "rock":
       return response
@@ -31,7 +40,10 @@ user_choice = ""
 while user_choice != "xxx":
   
   # Ask user for choice and check it's valid
-  user_choice = choice_checker("chose rock / paper / scissors (r/p/s): ")
+  user_choice = choice_checker ("chose rock / paper /"
+                                "scissors (r/p/s): ", rps_list,
+                                "please choose from rock / " 
+                                "paper / scissors (or xxx to quit) ")
 
 
   # Print out choice for comparison purpose
