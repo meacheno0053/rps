c26ea948a611056978341e85515602560a45ec54
meacheno0053 <99371795+meacheno0053@users.noreply.github.com>
2022-04-04 22:00:00 +0000

    error message included when calling funtion

---
+++ 01_RPS_Base_Component.py
@@ -0,0 +1,22 @@
+import random
+
+# Functions go here
+
+
+# Main routine goes here
+
+# Lists of valid responses
+yes_no_list = ["yes", "no"]
+rps_list = ["rock", "paper", "scisssors", "xxx"]
+
+# Ask user if they have played before.
+# If 'yes_no_listes', show instructions
+
+
+# Ask user for # of rounds then loop...
+
+
+# Ask user if they want to see their game history.
+# If 'yes' show game history
+
+#
--- 02_user_choice_v3.py
+++ 02_user_choice_v3.py
@@ -1,8 +1,8 @@
-# Version 2 - error message included when calling function
+# Version 3 - checks that response is in a given list
 
 
 # Funtions go here
-def choice_checker(question, error):
+def choice_checker(question, valid_list, error):
 
   valid = False
   while not valid:
@@ -10,21 +10,23 @@ def choice_checker(question, error):
     # Ask user for choice (and put choice in lowercase)
     response = input(question).lower()
 
-    if response == "r" or response == "rock":
-      return response
-    elif response == "p" or response == "paper":
-      return response
-    elif response == "s" or response == "scissors":
-      return response
+    #iterates through list and if response is an item
+    # in the lisit (or the first letter of an item), the
+    #full item name is returned
 
-    #check for exit code...
-    elif response == "xxx":
-      return response
-    else:
-      print(error)
-      
+    for item in valid_list:
+      if response == item[0] or response == item:
+        return response
+
+    #output error if item not in list
+    print(error)
+    print()
+    
 # Main routine goes here
 
+
+# list for checking responses
+rps_list = ["rock", "paper", "scissors", "xxx" ]
 
 # Loop for testing purposes
 user_choice = ""
@@ -32,7 +34,7 @@ while user_choice != "xxx":
   
   # Ask user for choice and check it's valid
   user_choice = choice_checker("chose rock / paper / "
-                               "scissors (r/p/s): ",
+                               "scissors (r/p/s): ", rps_list, 
                                "please chose form rock / paper / "
                                "scissors (or xxx to quit)")
  
