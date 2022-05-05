1f5344f3d96ef95d1bd7e11ff3168c45149231bf
meacheno0053 <99371795+meacheno0053@users.noreply.github.com>
2022-05-05 01:19:53 +0000

    starting my RPS scoring system

---
+++ 06_RPS_score.py
@@ -0,0 +1,34 @@
+# RPS Component 6 - scoring system 
+
+# Rounds won will be calculated (total - draw - lost)
+rounds_played = 0 
+rounds_lost = 0
+rounds_drawn = 0 
+
+# Results for testing purposes.
+test_results = ["won", "won", "loss", "loss", "tie"]
+
+# Play game
+for item in test_results:
+  rounds_played +=1
+
+  # Generate computer choice
+
+  result = item 
+
+  if result == "tie":
+    result = "It's a tie"
+    rounds_drawn += 1
+  elif result == "loss":
+    rounds_lost += 1 
+
+# Quick calculations (stats)
+rounds_won = rounds_played - rounds_lost - rounds_drawn
+
+# end of game starements
+print()
+print('***** End Game Summary *****')
+print("won: {} \t|\t Lost: {} \t|\t Draw: ") 
+"{}".format(rounds_won, rounds_lost, rounds_drawn))
+print()
+print("Thanks for playing")
